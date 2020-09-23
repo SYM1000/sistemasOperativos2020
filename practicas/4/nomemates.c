1e072515b0e34be20c5eb7309867f4f9f03b3dcc
SYM1000 <santiagoyeomansmolina@gmail.com>
2020-09-23 02:02:53 +0000

    final de la clase

---
+++ practicas/4/nomemates.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void signalHandler(int signum){
+    printf("HHAHAHHAA no me matas \n");
+}
+
+
+int main(){
+    signal(15, signalHandler);
+    while(1){
+        sleep(2);
+        printf("Trabajando \n");
+    }
+    return 0;
+}
